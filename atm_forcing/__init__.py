309131cbb2c116713d27c2b344eedd0e9dec3aff
sia <yakubov.sha@gmail.com>
2025-12-16 12:08:36 +0100

    Fix nora3 download in notebook

--- atm_forcing/__init__.py
+++ atm_forcing/__init__.py
@@ -1,1 +1,8 @@
-from .stuff import CF_ROMS, generate_catalog_urls, get_ds, regrid, reshape_to_full_year  # noqa: F401
+from .stuff import (  # noqa: F401
+    CF_ROMS,
+    generate_catalog_urls,
+    get_ds,
+    get_u_v_from_coords,
+    regrid,
+    reshape_to_full_year,
+)
